67e373932a3c2773d3bcbf0c92f61f92bfc001bc
Denes <denes@pop-os.home>
2025-07-28 11:46:17 +0100

    Added js logic for interactivity

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+const canvas = document.getElementById("canvas");
+const canvasContainer = document.querySelector(".canvasContainer");
+
+const ctx = canvas.getContext('2d');
+let image = new Image();
+
+document.getElementById("upload").addEventListener("change", function(event){
+    const file = event.target.file[0];
+    const reader = new FileReader();
+    reader.onload = function(e){
+        image.src = e.target.result;
+    }
+
+    reader.readAsDataURL(file)
+})
+
+image.onload = function(){
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    ctx.drawImage(image, 0, 0);
+    canvasContainer.computedStyleMap.display = 'block';
+}
+
+function applyFileters(){
+    const brightness = document.getElementById("brightness").value;
+    const contrast = document.getElementById("contrast").value;
+    const grayscale = document.getElementById("greyscale").value;
+
+    ctx.filter = `
+    brightness(${brightness})
+    contrast(${contrast})
+    grayscale(${grayscale})`;
+
+    ctx.drawImage(image, 0, 0);
+}
+
+document.getElementById("brightness").addEventListener("input", applyFileters);
+document.getElementById("contrast").addEventListener("input", applyFileters);
+document.getElementById("grayscale").addEventListener("input", applyFileters);
+
+document.getElementById("download").addEventListener("click", function(){
+    const link = document.createElement("a");
+    link.href = canvas.toDataURL();
+    link.download = 'edited-imae.png';
+
+    link.click();
+})
